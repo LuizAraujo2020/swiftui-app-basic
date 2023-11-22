7335b8be2304bf226d3ed4dd03bd75efffca995c
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-11-22 09:06:34 -0300

    feat: app icon

---
+++ LearnByDoing/LearnByDoing/LearnByDoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnByDoingApp.swift
+//  LearnByDoing
+//
+//  Created by Luiz Araujo on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnByDoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
